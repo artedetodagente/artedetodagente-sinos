b81c27968fc93ce95225a93eafd6caa27c11aeb7
Marlus Araujo <m@marlus.com>
2020-07-13 23:30:17 -0300

    cssnano + tailwind purge

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 //postcss.config.js
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('tailwindcss')('./tailwind.config.js'),
     require('postcss-node-sass'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('cssnano')
   ],
 };
