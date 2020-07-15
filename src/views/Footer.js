b16537030d8a451ee0ab79895b91b29bc2a6a425
Marlus Araujo <m@marlus.com>
2020-07-14 21:33:24 -0300

    swiper + dropdown integration +  social icons

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -5,7 +5,33 @@ function Footer(props) {
 
   return (
     <footer className="footer">
-      this is the footer
+      <div className="footer-1">
+        <div className="footer-col social">
+          <h3>Social</h3>
+          <a href="https://instagram.com/sinos" target="_blank" title="Instagram">
+            <img width="20" src="/img/icons/instagram.svg" alt="Instagram" />
+          </a>
+          <a href="https://facebook.com/sinos" target="_blank" title="Facebook">
+            <img width="20" src="/img/icons/facebook.svg" alt="Facebook" />
+          </a>
+          <a href="https://youtube.com/sinos" target="_blank" title="Youtube">
+            <img width="20" src="/img/icons/youtube.svg" alt="Youtube" />
+          </a>
+        </div>
+        <div className="spacer md:flex-grow"></div>
+        <div className="footer-col">
+          <h3>Institucional</h3>
+        </div>
+        <div className="footer-col">
+          <h3>Acesso</h3>
+        </div>
+        <div className="footer-col">
+          <h3>Contatos</h3>
+        </div>
+      </div>
+      <div className="footer-2">
+        second footer
+      </div>
     </footer>
   );
 }
--- src/views/HomeCurso.js
+++ src/views/HomeCurso.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { HashLink as Link } from 'react-router-hash-link'
+import { set } from 'ramda'
 
 function HomeCurso(props) {
 
   const {data} = props
 
+  const [dropIsDown,setDrop] = useState(false)
+  const [selected,setSelected] = useState(0)
+  const [slideTo, setSlideTo] = useState(null)
+
+  const current = data.modules[selected]
+
+  // swiper events
+  const onSlide = (e) => setSelected(e.realIndex)
+  const bindSwiper = (swiper) => setSlideTo(() => x => swiper.slideToLoop(x))
+
+  // dropdown events
+  const dropToggle = () => setDrop(!dropIsDown)
+  const dropSelect = (i) => () => {
+    if(dropIsDown){
+      setDrop(false)
+    }
+    setSelected(i)
+    slideTo(i)
+  }
+
   return (
+
     <section id={data.id} className="home-curso full-section">
+
       <div className="curso-swiper">
         <Swiper
           spaceBetween={50}
           loop={true}
-          // slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={onSlide}
+          onSwiper={bindSwiper}
         >
           {data.modules.map((m,i)=>{
             return (
@@ -25,21 +47,36 @@ function HomeCurso(props) {
           })}
         </Swiper>
       </div>
+
       <div className="curso-info">
+
         <div className="header">
           <div className="title" style={{backgroundColor: data.color}}>{data.title}</div>
-          <div className="dropdown">Dropdown</div>
+          <div className={`dropdown ${dropIsDown ? 'isdown' : ''}`}>
+            <div className="selected" onClick={dropToggle}>{current.title}</div>
+            <div className="options-viewport">
+              <div className="options">
+              {data.modules.map((m,i)=>{
+                return <li key={`${data.id}-drop-${i}`} onClick={dropSelect(i)}>{m.title}</li>
+              })}
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className="content">
           <div className="text">{data.text}</div>
-          <div className="counter">6 modulos</div>
+          <div className="counter">{data.modules.length} módulos</div>
         </div>
+
         <div className="acessar">
-          <Link to={`/cursos/${data.id}`}>Acessar</Link>
+          <Link to={`/cursos/${data.id}/${current.id}`}>Acessar</Link>
         </div>
+
       </div>
+      
     </section>
-  );
+  )
 }
 
-export default HomeCurso;
+export default HomeCurso
