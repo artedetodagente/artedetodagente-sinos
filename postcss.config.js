efb77b7e207763fc9c6f9edbe1aebdb430804674
Marlus Araujo <m@marlus.com>
2020-07-16 01:56:01 -0300

    sass => postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 //postcss.config.js
 module.exports = {
   plugins: [
+    require('postcss-import'),
     require('tailwindcss')('./tailwind.config.js'),
-    require('postcss-node-sass'),
+    require('postcss-nested'),
     require('autoprefixer'),
     require('cssnano')
   ],
