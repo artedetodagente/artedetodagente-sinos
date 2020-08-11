7224c66b801807439a312af8daf71e6ca5eaf5d5
Marlus Araujo <m@marlus.com>
2020-08-11 13:19:38 -0300

    menu hide items

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -10,9 +10,9 @@ function Menu(props) {
       <li><Link smooth to="/#projetoespiral">Projeto Espiral</Link></li>
       <li><Link smooth to="/#academiaderegencia">Academia de Regência</Link></li>
       <li><Link smooth to="/#eorquestras">E-Orquestras</Link></li>
-      <li><Link smooth to="/publicacoes">Publicações</Link></li>
+      {/* <li><Link smooth to="/publicacoes">Publicações</Link></li> */}
       <li><Link smooth to="/noticias">Notícias</Link></li>
-      <li><Link smooth to="/galeria">Galeria</Link></li>
+      {/* <li><Link smooth to="/galeria">Galeria</Link></li> */}
       <li><Link smooth to="/parceiros">Parceiros</Link></li>
       <li><Link smooth to="/imprensa">Imprensa</Link></li>
     </>
