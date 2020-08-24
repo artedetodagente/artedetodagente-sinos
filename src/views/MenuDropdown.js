383841a67d0300def70b6d51ef7ea9fa9765640d
Marlus Araujo <m@marlus.com>
2020-08-24 17:13:35 -0300

    menu interno

--- src/views/MenuDropdown.js
+++ src/views/MenuDropdown.js
@@ -10,7 +10,7 @@ function MenuDropdown(props) {
   return (
     <div className="menu-dropdown">
       <div className="menu-toggle" onClick={() => setOpen(!open)}>
-        <img src="/img/icons/menu.svg" style={{width:"48px", height: "45px"}} alt="menu" />
+        <img src="/img/icons/menu.svg" style={{width:"48px", height: "46px"}} alt="menu" />
       </div>
       <div className={`menu-content ${open ? 'is-open' : '' }`}>
         <div onClick={() => setOpen(false)}  className="site-menu-1 bg-r">
--- src/views/PageDefault.js
+++ src/views/PageDefault.js
@@ -9,16 +9,9 @@ function PageDefault(props) {
   return (
     <>
       <section id={props.id} className="single-page">
-        <div className="mobile-menu">
+        <div className="header">
           <MenuDropdown home={true}/>
-        </div>
-        <div className="header-vireport">
-          <div className="header">
-            <div className="title">{props.title}</div>
-            <div className="desktop-menu site-menu-1 bg-r">
-              <Menu home={true} />
-            </div>
-          </div>
+          <div className="title">{props.title}</div>
         </div>
         <div className="content-viewport">
           <div className="content">
