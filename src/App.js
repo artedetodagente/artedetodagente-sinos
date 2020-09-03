7d53d61a50b8e2903c63f16ba434982e5905e50f
Marlus Araujo <m@marlus.com>
2020-09-03 15:22:36 -0300

    Page Imprensa

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PageCursos from './views/PageCursos'
 import PageNoticias from './views/PageNoticias'
 import PageDefault from './views/PageDefault'
 import PageAgenda from './views/PageAgenda'
+import PageImprensa from './views/PageImprensa'
 import ScrollToTop from './util/ScrollToTop'
 
 function App() {
@@ -24,6 +25,9 @@ function App() {
         </Route>
         <Route path="/programacao">
           <PageAgenda />
+        </Route>
+        <Route path="/imprensa">
+          <PageImprensa />
         </Route>
         <Route exact path="/:id">
           <PageDefault />
