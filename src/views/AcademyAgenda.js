6d6f7334094ae7be4be6fd6412a913e405421587
Marlus Araujo <m@marlus.com>
2020-10-02 20:42:44 -0300

    AVS bugfix

--- src/views/AcademyAgenda.js
+++ src/views/AcademyAgenda.js
@@ -8,56 +8,46 @@ import api from '../services/api'
 
 export default function AcademyAgenda(props){
 
-    const {slug} = useParams()
+  const {slug} = useParams()
+  const [projetos, setProjetos] = useState([])
 
-    const [projeto, setProjeto] = useState([])
-    const [projetos, setProjetos] = useState([])
+  let history = useHistory()
 
-    let history = useHistory()
-
-    useEffect(()=>{
-        const fetchData = async () => {
-            const response = await api.get(`/projetos/${slug}`)
-            const responseProjetos = await api.get('/projetos')
-            setProjeto(response.data)
-            setProjetos(responseProjetos.data.slice(0,2))
-        }
-        fetchData()
-    },[slug])
-
-    
-    function dropSelect (project) {
-      history.push(`/academyAgenda/${project.slug}`)
+  useEffect(()=>{
+    const fetchData = async () => {
+      const responseProjetos = await api.get('/projetos')
+      setProjetos(responseProjetos.data.slice(0,2))
     }
+    fetchData()
+  },[])
 
-    // console.log([`slug`,R.findIndex(R.propEq('slug', slug),projetos)])
+  const onSelect = (i) => history.push(`/academyAgenda/${projetos[i].slug}`)
+  const selected = R.findIndex(R.propEq('slug', slug), projetos)
+  const options = projetos.map((proj,i) => {
+    return {
+      title: proj.title,
+      background: proj.color,
+      color: '#fff'
+    }
+  })
 
-    return(
-        <Page title="Academia Virtual Sinos" id="page-avs">
-          <div id="box">
-            <div id="box-title">
-              <h3>
-                Programação
-              </h3>
-            </div>
-            {projetos &&
-            <DropDown
-              selected={R.findIndex(R.propEq('slug', slug),projetos)}
-              onSelect={(i)=>dropSelect(projetos[i])}
-              options={projetos.map((proj,i) => {
-                return {title: proj.title, background: proj.color, color: '#fff'}
-              })}
-            />
-            }
-          </div>
-          <AcademyPageAgenda/>
-          <div id="form-box">
-            {projeto.form_link ? 
-              <a href={projeto.form_link} target="__blank">
-                    Clique aqui para fazer sua inscrição
-              </a>
-            : null}
-          </div>
-        </Page>
-    )
+  return(
+    <Page title="Academia Virtual Sinos" id="page-avs">
+      <div id="box">
+        <div id="box-title">
+          <h3>
+            Programação
+          </h3>
+        </div>
+        {projetos &&
+        <DropDown
+          selected={selected}
+          onSelect={onSelect}
+          options={options}
+        />
+        }
+      </div>
+      <AcademyPageAgenda/>
+    </Page>
+  )
 }
--- src/views/AcademyPageAgenda.js
+++ src/views/AcademyPageAgenda.js
@@ -1,11 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import {
-    useParams,
-    Switch,
-    Route,
-    useRouteMatch,
-    } from 'react-router-dom'
-
+  useParams,
+  Switch,
+  Route,
+  useRouteMatch,
+  } from 'react-router-dom'
+import * as R from 'ramda'
 import api from '../services/api'
 import {fdate} from '../util'
 
@@ -13,102 +13,99 @@ import parse from 'html-react-parser'
 
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
-export default function AcademyPageAgenda({id}){
+export default function AcademyPageAgenda(){
 
-    const [current_month, setCurrentMonth] = useState(new Date().getMonth())
+  const {slug} = useParams()
+  const {path} = useRouteMatch()
 
-    const {slug} = useParams()
+  const [schedules, setSchedules] = useState([])
+  const [projeto, setProjeto] = useState([])
+  const [events, setEvents] = useState([])
+  const [index, setIndex] = useState()
 
-    let { path } = useRouteMatch()
+  useEffect(()=>{
+    async function fetchData(){
+      const response = await api.get(`/projetos/${slug}`)
+      setIndex(-1)
+      setSchedules(response.data.avs_schedules)
+      setProjeto(response.data)
+    }
+    fetchData()
+  },[slug])
 
-    const [schedules, setSchedules] = useState([])
-    const [projeto, setProjeto] = useState([])
-    const [month, setMonth] = useState([])
-    const [events, setEvents] = useState([])
+  useEffect(()=>{
+    const todayMonth = new Date().getMonth()
+    const index = R.findIndex(R.propEq('month', months[todayMonth]), schedules)
+    if(index !== undefined && index !== -1){
+      setIndex(index)
+    }
+  },[schedules])
+  
+  useEffect(()=>{
+    if(schedules && index !== undefined && index !== -1){
+      setEvents(schedules[index].Days)
+    }
+  },[schedules,index])
 
-    useEffect(()=>{
-        async function fetchData(){
-            const response = await api.get(`/projetos/${slug}`)
-            setSchedules(response.data.avs_schedules)
-            setProjeto(response.data)
-        }
-        fetchData()
-    },[slug])
+  const prevEnabled = index > 0
+  const nextEnabled = index < schedules.length - 1
 
-    useEffect(()=>{
-        async function fetchData(){
-            const responseSchedule = await api.get(`/avs-schedules?month=${months[current_month]}&projeto.slug_eq=${slug}`)
-            if(responseSchedule.data[0]){
-              setMonth(responseSchedule.data[0])
-              setEvents(responseSchedule.data[0].Days)
-            }else{
-              const response = await api.get(`/avs-schedules?month=${months[current_month-1]}&projeto.slug_eq=${slug}`)
-              setMonth(response.data[0])
-              setEvents(response.data[0].Days)
-            }
-        }
-        fetchData()
-    },[slug,current_month])
+  function nextMonth() {
+    if(nextEnabled) setIndex(index + 1)
+  }
+  
+  function prevMonth() {
+    if(prevEnabled) setIndex(index - 1)
+  }
 
-     var index
+  return (
+    <>
+      <div className="page-view agenda-view">
+        <Switch>
+          <Route path={path}>
+            <nav className="agenda-nav">
+              <button
+                onClick={()=>prevMonth()}
+                style={{opacity: prevEnabled ? 1 : 0.5}}
+              >◀</button>
+              <span>{schedules[index]?.month || `...`}</span>
+              <button
+                onClick={()=>nextMonth()}
+                style={{opacity: nextEnabled ? 1 : 0.5}}
+              >▶</button>
+            </nav>
 
-      
-      for(const schedule of schedules){
-        if(schedule.month === month.month){
-          index = schedules.indexOf(schedule)
-        }
-      }
+            <p>&nbsp;</p>
 
-      const prevEnabled = index > 0
-      const nextEnabled = index < schedules.length - 1
-
-      function nextMonth() {
-        if(nextEnabled) setCurrentMonth(current_month + 1)
-      }
-    
-      function prevMonth() {
-        if(prevEnabled) setCurrentMonth(current_month - 1)
-      }
-
-    return (
-        <div className="page-view agenda-view">
-            <Switch>
-                <Route path={path}>
-                  <nav className="agenda-nav">
-                    <button
-                      onClick={()=>prevMonth()}
-                      style={{opacity: prevEnabled ? 1 : 0.5}}
-                    >◀</button>
-                    <span>{month.month || `...`}</span>
-                    <button
-                      onClick={()=>nextMonth()}
-                      style={{opacity: nextEnabled ? 1 : 0.5}}
-                    >▶</button>
-                </nav>
-
-                <p>&nbsp;</p>
-
-                <div className="agenda-feed">
-                  {events.map((evento,i) => {
-                    const date = fdate(evento.date)
-                      return (
-                        <article key={`agenda-${current_month}-${i}`}>
-                          <div className="agenda-item">
-                            <div className="agenda-date">
-                              <h3 style={{color: `${projeto.color}`}}>{date.day}</h3>
-                              <p>{date.month}</p>
-                            </div>
-                            <div className="agenda-content">
-                              <h3 style={{color: projeto.color}}>{evento.time} | {evento.title}</h3>
-                              <div>{parse(evento.description)}</div>
-                            </div>
-                          </div>
-                        </article>
-                      )
-                    })}
-                </div>
-              </Route>
-            </Switch>
-        </div>
-    )
+            <div className="agenda-feed">
+              {events && events.map((evento,i) => {
+                const date = fdate(evento.date)
+                  return (
+                    <article key={`agenda-${index}-${i}`}>
+                      <div className="agenda-item">
+                        <div className="agenda-date">
+                          <h3 style={{color: `${projeto.color}`}}>{date.day}</h3>
+                          <p>{date.month}</p>
+                        </div>
+                        <div className="agenda-content">
+                          <h3 style={{color: projeto.color}}>{evento.time} | {evento.title}</h3>
+                          <div>{parse(evento.description)}</div>
+                        </div>
+                      </div>
+                    </article>
+                  )
+                })}
+            </div>
+          </Route>
+        </Switch>
+      </div>
+      <div id="form-box">
+        {projeto.form_link ? 
+          <a href={projeto.form_link} target="__blank">
+                Clique aqui para fazer sua inscrição
+          </a>
+        : null}
+      </div>
+    </>
+  )
 }
