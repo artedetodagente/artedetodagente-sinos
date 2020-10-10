ac02cb8b51b0697be97906cb2d9f6f916475b72e
Marlus Araujo <m@marlus.com>
2020-10-10 00:11:47 -0300

    PageNoticias split in two components

--- src/views/PageNoticias.js
+++ src/views/PageNoticias.js
@@ -1,27 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import ReactMarkdown from 'react-markdown'
-
-
-import api from '../services/api'
-
-import {
-  Switch,
-  Route,
-  useParams,
-  useRouteMatch
-} from "react-router-dom"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
 import { HashLink as Link } from 'react-router-hash-link'
-import {fdate} from '../util'
-
 import slugify from 'slugify'
 
+import api from '../services/api'
+import { fdate } from '../util'
+
 import Page from './Page'
+import PageNoticiasDetalhe from './PageNoticiasDetalhe'
 
 function PageNoticias() {
 
-  let { path } = useRouteMatch()
+  let {path} = useRouteMatch()
 
-  const [ noticias, setNoticias ] = useState([])
+  const [noticias,setNoticias] = useState([])
 
   useEffect(()=>{
     async function fetchData(){
@@ -36,71 +28,42 @@ function PageNoticias() {
     <Page title="Notícias">
       <div className="page-view posts-view">
         <Switch>
-
           <Route exact path={path}>
-            <div className="title-1">
-              <span><Link to={`/`}>SINOS</Link> &raquo;&nbsp;</span>
-              <span>Notícias</span>
-            </div>
-            <p>&nbsp;</p>
-            <div className="posts-feed">
-            {noticias.map((noticia,i) => {
-                const date = fdate(noticia.date)
-                const foto = noticia.pic
-                return (
-                  <article className="post" key={`noticia-${i}`}>
-                    <Link className="post-image" to={`/noticias/${noticia.id}/${slugify(noticia.title)}`}>
-                      <img alt={noticia.title} src={`https://admin.sinos.art.br${foto.url}`} />
-                    </Link>
-                    <Link className="post-title" to={`/noticias/${noticia.id}/${slugify(noticia.title)}`}>{noticia.title}</Link>
-                    <p className="post-text">{noticia.call}</p>
-                    <p className="post-date">Publicado em {date.day} de {date.month} de {date.year}</p>
-                  </article>
-                )
-              }
-            )}
-            </div>
+            <PageNoticiasMain noticias={noticias} />
           </Route>
-
           <Route path={`${path}/:postid`}>
-            <Noticia />
+            <PageNoticiasDetalhe />
           </Route>
-
         </Switch>
       </div>
     </Page>
   );
 }
 
-function Noticia(props) {
-
-  const {postid} = useParams()
-  const [noticia, setNoticia] = useState([])
-  const [date, setDate] = useState([])
-
-  useEffect(()=>{
-    async function fetchData(){
-      const response = await api.get(`/noticias/${postid}`)
-      setDate(fdate(response.data.date))
-      setNoticia(response.data)
-    }
-    fetchData()
-  },[postid])
-  const foto = noticia.pic
-
-  return (
+function PageNoticiasMain ({noticias}) {
+  return(
     <>
       <div className="title-1">
         <span><Link to={`/`}>SINOS</Link> &raquo;&nbsp;</span>
-        <span><Link to={`/noticias`}>Notícias</Link> &raquo;&nbsp;</span>
-        <span>{noticia.title}</span>
+        <span>Notícias</span>
       </div>
-      {foto ? <img alt={noticia.title} src={`https://admin.sinos.art.br${foto.url}`} width="50%" style={{float:'right', margin: '40px 0 40px 40px'}} /> : null}
-      <ReactMarkdown
-        source={noticia.description}
-      />
       <p>&nbsp;</p>
-      {<p className="post-date">Publicado em {date.day} de {date.month} de {date.year}</p>}
+      <div className="posts-feed">
+        {noticias.map((noticia,i) => {
+          const date = fdate(noticia.date)
+          const foto = noticia.pic
+          return (
+            <article className="post" key={`noticia-${i}`}>
+              <Link className="post-image" to={`/noticias/${noticia.id}/${slugify(noticia.title)}`}>
+                <img alt={noticia.title} src={`https://admin.sinos.art.br${foto.url}`} />
+              </Link>
+              <Link className="post-title" to={`/noticias/${noticia.id}/${slugify(noticia.title)}`}>{noticia.title}</Link>
+              <p className="post-text">{noticia.call}</p>
+              <p className="post-date">Publicado em {date.day} de {date.month} de {date.year}</p>
+            </article>
+          )
+        })}
+      </div>
     </>
   )
 }
