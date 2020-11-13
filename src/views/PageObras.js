f3b3c975f18348334d6589e84f10e9b856892304
Wesley Lima <wesleyjoga500@gmail.com>
2020-11-13 19:27:24 -0300

    ajustes

--- src/views/PageObras.js
+++ src/views/PageObras.js
@@ -127,7 +127,13 @@ function Obra({ path }){
       }
     }
 
-    console.log(partitura)
+    const size = useWindowSize()
+
+
+    console.log(size)
+
+    const a = true;
+
     return (
         <div>
           <div className="links">
@@ -177,7 +183,12 @@ function Obra({ path }){
                     <p className="repertorio-title"><strong>Tempo: {obra.minutagem}</strong></p>
                 </div>
             </div>
-            <div className="partituras-container">
+            <div className="partituras-container">  
+              <div className="obra-buttons">
+                      <button onClick={()=>previousPage()}><ArrowBackIos/></button>
+                        <p>Página {pageNumber} de {numPages}</p>
+                      <button onClick={()=>nextPage()}><ArrowForwardIos/></button>            
+                </div>   
               { partitura ? 
                 <Document
                     className="pdf"
@@ -185,18 +196,45 @@ function Obra({ path }){
                     loading="Carregando PDF..."
                     file={`https://admin.sinos.art.br${partitura.url}`}
                     onLoadSuccess={onDocumentLoadSuccess}
+                    renderMode="svg"
                     >
-                    <Pager pageNumber={pageNumber} />
-                    <div className="obra-buttons">
-                      <button onClick={()=>previousPage()}><ArrowBackIos/></button>
-                        <p>Página {pageNumber} de {numPages}</p>
-                      <button onClick={()=>nextPage()}><ArrowForwardIos/></button>            
-                    </div>
+                    <Pager pageNumber={pageNumber} scale={size.width <= 400 ? 0.5 : 1.7}>
+                    </Pager>
                     </Document>
               : "Não há PDF"}
-                  
             </div>
           </div>
       </div>
     )
+}
+
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+
+  return windowSize;
 }
