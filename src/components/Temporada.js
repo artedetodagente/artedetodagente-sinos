4b5c3c5cddc8edb46d6a6c863a9953f723b80594
Eduardo Mangeli <eduardo@mangeli.com.br>
2021-05-29 11:56:22 -0300

    separação de concertos por temporada

---
+++ src/components/Temporada.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import CardConcerto from './CardConcerto';
+//import { Container } from './CardObraStyles';
+import { ObrasContainer } from './ObraStyles'
+import { TemporadaContainer } from './TemporadaStyles';
+
+export default function Temporada({ temporada, concertos}) {
+  return (
+    <TemporadaContainer>
+    <p>Temporada {temporada}</p>
+    <ObrasContainer>
+    {
+      concertos.map( (concerto, i) => {
+        return (
+          <Link to={`/concertos-sinos/${concerto.slug}`} key={i}>
+            <CardConcerto concerto={concerto}/>
+          </Link>
+        )}
+      )
+    }
+    </ObrasContainer>
+    </TemporadaContainer>
+  )
+};
---
+++ src/components/TemporadaStyles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const TemporadaContainer = styled.div`
+  > p {  
+    font-size: 2.25em;
+    //font-weight: 500;
+    color: black;
+    margin-top: 2em;
+    text-transform: uppercase;
+  }
+`;
+
+export {TemporadaContainer};
